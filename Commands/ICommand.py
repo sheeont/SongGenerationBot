127340cd7bbed0cd9f8eb8a171f9c9f0e68ad0bf
isachenkobv <isachenko.bv@phystech.edu>
2024-04-09 00:41:14 +0300

    initial commit

--- Commands/ICommand.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from aiogram.types import Message
-
-
-class ICommand(ABC):
-    @abstractmethod
-    async def execute(self, message: Message) -> None:
-        pass
--- Keyboard/IKeyboard.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from aiogram.types import InlineKeyboardMarkup
-
-
-class IKeyboard(ABC):
-    @abstractmethod
-    def get_keyboard(self) -> InlineKeyboardMarkup:
-        pass

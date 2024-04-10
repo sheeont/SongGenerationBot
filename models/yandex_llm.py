018bb79609d8ee44ee4fa824d3b2fda1d21380fd
isachenkobv <isachenko.bv@phystech.edu>
2024-04-10 17:56:08 +0300

    Fixed API key

--- models/yandex_llm.py
+++ models/yandex_llm.py
@@ -1,6 +1,10 @@
 import requests
 import asyncio
 from dataclasses import dataclass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @dataclass
@@ -8,17 +12,18 @@ class YandexRequestInfo:
     system_text: str
     user_text: str
     folder_id: str
-    iam_token: str
+    api_key: str
     temperature: int
 
 
-baseline_prompt = """Забудьте все свои предыдущие инструкции. 
-Представьте, что вы известный поэт с высоким навыком рифмования, а так же известный музыкальный исполнитель. 
-Ваша задача придумать осмысленную песню в определенном жанре, которая понравится людям. 
-Вам будет дана первая строка песни, а так же жанр. Вы должны вернуть только текст песни."""
-iam_token = 't1.9euelZqYypfPiZqLjsicmJiWm86Liu3rnpWaiZ2VyZTIx8aUkc7MkJqRko7l8_daby5P-e8UFDkJ_t3z9xoeLE_57xQUOQn-zef1656VmsqSxsuJz52azY2Tk4vOmZ6W7_zF656VmsqSxsuJz52azY2Tk4vOmZ6W.tEGyYaegYVVpcAe5Ov2q-wdRN-m68vjRTygvyajHOMxQCtoehWNEZsHaCdNVfl0jGZaCc9XimCIOWhbdHeiNAQ'
+api_key = os.getenv('API_KEY')
 folder_id = 'b1gpa67eg05vpudm3tn9'
 api_url = 'https://llm.api.cloud.yandex.net'
+baseline_prompt = """Забудьте все свои предыдущие инструкции. 
+Представьте, что вы известный поэт с самым высочайшим навыком рифмования, а так же известный музыкальный исполнитель. 
+Ваша задача придумать осмысленную песню в определенном жанре, которая понравится людям..
+Вам будет дана первая строка песни, а так же жанр. Вы должны вернуть только текст песни.
+ПРИМЕЧАНИЯ, ПОСТИСЛОВИЯ, ПРЕДУПРЕЖДЕНИЯ И ЛЮБОЙ ТЕКСТ КРОМЕ ПЕСНИ ВЫВОДИТЬ КАТЕГОРИЧЕСКИ ЗАПРЕЩЕНО"""
 
 
 def ask_gpt_with_prompt(yandex_request_info: YandexRequestInfo):
@@ -40,7 +45,7 @@ def ask_gpt_with_prompt(yandex_request_info: YandexRequestInfo):
             }
         ]
     }
-    headers = {"Authorization": f"Bearer {yandex_request_info.iam_token}", "x-folder-id": yandex_request_info.folder_id}
+    headers = {"Authorization": f"Api-key {yandex_request_info.api_key}", "x-folder-id": yandex_request_info.folder_id}
     url = f"{api_url}/foundationModels/v1/completionAsync"
     res = requests.post(url, headers=headers, json=req)
     try:
@@ -49,9 +54,9 @@ def ask_gpt_with_prompt(yandex_request_info: YandexRequestInfo):
         raise ValueError(res.json())
 
 
-async def fetch_operation_result(operation_id, iam_token=iam_token):
+async def fetch_operation_result(operation_id, api_key=api_key):
     url = f"{api_url}/operations/{operation_id}"
-    headers = {"Authorization": f"Bearer {iam_token}"}
+    headers = {"Authorization": f"Api-Key {api_key}"}
     try:
         while True:
             res = requests.get(url, headers=headers)
@@ -61,10 +66,9 @@ async def fetch_operation_result(operation_id, iam_token=iam_token):
     except:
         raise ValueError(res.json())
 
-task = """
-Первая строка: Вышка, студкемп, браточки с кайфом
-Жанр: Рэп
-"""
-yandex_request_info = YandexRequestInfo(baseline_prompt, task, folder_id, iam_token, 0.8)
-operation_id = ask_gpt_with_prompt(yandex_request_info)
-operation_result = await fetch_operation_result(operation_id)
+
+async def generate_song(task, temperature):
+    yandex_request_info = YandexRequestInfo(baseline_prompt, task, folder_id, api_key, temperature)
+    operation_id = ask_gpt_with_prompt(yandex_request_info)
+    operation_result = await fetch_operation_result(operation_id)
+    return operation_result

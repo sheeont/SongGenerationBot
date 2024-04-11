a1facb8a933ba7288148c8a7dbc66e22e71c242a
iqbara <intel7ligents@gmail.com>
2024-04-11 15:43:36 +0300

    Integrate Text Generation

--- bot/Content/TextContentGenerator.py
+++ bot/Content/TextContentGenerator.py
@@ -5,6 +5,7 @@ from models import yandex_llm as gen
 
 from bot.Content import IContentGenerator
 from bot.config import text_model_temperature
+from bot.Utils import Utils
 
 
 class TextContentGenerator(IContentGenerator):
@@ -14,9 +15,11 @@ class TextContentGenerator(IContentGenerator):
 
     async def generate_content(self, initial_text: str) -> str:
         task = f"Первая строка: {self.initial_text}\nЖанр: {self.style_type}"
+
         try:
             timeout_seconds = 60
-            generated_text = await asyncio.wait_for(gen.generate_song(task, text_model_temperature), timeout_seconds)
+            generated_text: str = await asyncio.wait_for(gen.generate_song(task, text_model_temperature/10), timeout_seconds)
+            generated_text = Utils.replace_bold(generated_text)
             return generated_text
         except asyncio.TimeoutError:
             logging.error("TimeoutError: Превышено время ожидания генерации текста.")

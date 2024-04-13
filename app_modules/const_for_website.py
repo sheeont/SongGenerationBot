be7046184ab085020f96b39e083e683fe6cf0a20
sheeont <nikolaevgrisha@inbox.ru>
2024-04-13 06:39:11 +0300

    Streamlit error

--- app_modules/const_for_website.py
+++ app_modules/const_for_website.py
@@ -1,5 +1,5 @@
 genre = ('Рок', 'Шансон', 'Рэп', 'Поп')
 genre_en = ('Rock', 'chanson', 'rap', 'pop_music')
-models = ('Yandex GPT', 'RuGPT2', 'RNN')
+models = ('Yandex GPT', 'RuGPT2')
 title = 'GL HF GG'
-range_for_us_picking =[i for i in range(1, 11)]
+range_for_us_picking = [i for i in range(1, 11)]
